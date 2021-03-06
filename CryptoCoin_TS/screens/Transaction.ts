768ae96248dbc328ef75af2c5df4a9635b665f9f
AgostonAttila <atesz014@gmail.com>
2021-03-06 18:16:04 +0100

    CryptoCoin init

---
+++ CryptoCoin_TS/screens/Transaction.ts
@@ -0,0 +1,29 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+export default function Transaction() {
+  return (
+    <View style={styles.container}>
+      <Text>Transaction</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+
+    elevation: 8,
+  },
+});
